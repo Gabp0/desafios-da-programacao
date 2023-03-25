f97d1bf1de0a41948530ca7381d69f776d37a964
gab <gabriel.pontarolo045@gmail.com>
2023-03-25 15:04:32 -0300

    sofrimento semana 1 pt 2

---
+++ src/1D_poli.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+long long pinf = 9223372036854775807;
+long long minf = -9223372036854775807;
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    // code
+    ll n;
+    string rna;
+
+    cin >> n >> ws;
+    cin >> rna >> ws;
+
+    ull s = rna.find("AAAAAAA");
+    ull e = rna.rfind("AAAAAAA");
+    if ((e == string::npos) | (s == string::npos))
+    {
+        cout << 0 << "\n";
+    }
+    else
+    {
+        cout << (e - s + 7) << endl;
+    }
+}
---
+++ src/1E_esperanto.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+long long pinf = 9223372036854775807;
+long long minf = -9223372036854775807;
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    // code
+
+    ll n;
+    string word;
+
+    cin >> n >> ws;
+    cin >> word >> ws;
+
+    if (word.back() == 's')
+    {
+        cout << word.at(n - 2) << word.at(n - 1) << "\n";
+    }
+    else
+    {
+        cout << word.back() << "\n";
+    }
+}
