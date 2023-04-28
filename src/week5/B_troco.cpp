1711164b7908c7b2cb05ae5bb734765087654c3f
gab <gabriel.pontarolo045@gmail.com>
2023-04-28 12:42:21 -0300

    troco

--- src/week5/B_troco.cpp
+++ src/week5/B_troco.cpp
@@ -12,27 +12,36 @@ using us = unsigned short;
 long long pinf = 9223372036854775807;
 long long minf = -9223372036854775807;
 
-int n;
 vector<int> coins = {50, 25, 10, 5, 1};
+
 int main()
 {
     cin.tie(0);
     ios_base::sync_with_stdio(0);
 
     // code
-    ll x;
-    while (cin >> x)
-    {
-        vector<ll> dp(n + 1, 0);
-        dp[0] = 1;
+    vector<vector<ull>> dp(30001, vector<ull>(5, 0));
 
-        for (auto c : coins)
+    ll x, y;
+
+    for (int i = 0; i < 5; i++)
+    {
+        dp[0][i] = 1;
+    }
+
+    for (ll i = 1; i < 30001; i++)
+    {
+        for (int j = 0; j < 5; j++)
         {
-            for (int i = c; i <= n; i++)
-            {
-                dp[i] += dp[i - c];
-            }
+            x = (i - coins[j] >= 0) ? dp[i - coins[j]][j] : 0;
+            y = (j >= 1) ? dp[i][j - 1] : 0;
+            dp[i][j] = x + y;
         }
-        cout << dp[n] << "\n";
+    }
+
+    ll n;
+    while (cin >> n)
+    {
+        cout << dp[n][4] << "\n";
     }
 }
